79a552d476bd911eb5c47831c1060afa8122f5d2
Simon-Richelle <47533879+Simon-Richelle@users.noreply.github.com>
2019-08-28 20:36:50 +0200

    Update GCP-VM_setup.sh

--- GCP-VM_setup.sh
+++ GCP-VM_setup.sh
@@ -17,7 +17,7 @@ git clone <XXXXX> #cloning github repository
 
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y -qq --fix-missing install python3-mpltoolkits.basemap python3-numpy python3-matplotlib python3-requests
+sudo apt-get -y -qq --fix-missing install python3-mpltoolkits.basemap python3-numpy python3-matplotlib python3-requests python3-pandas
 
 ### HOW TO :
 # bash <XXX>.sh #run a bash file called <XXX>
